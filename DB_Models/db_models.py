8b3e0a19ef6c4d3b27aa2f037782bea02ba85617
sriramg89 <37271647+sriramg89@users.noreply.github.com>
2021-02-23 19:19:51 +0530

    Formatted code to pep8

--- DB_Models/db_models.py
+++ DB_Models/db_models.py
@@ -8,53 +8,55 @@ from sqlalchemy.sql.expression import null, true
 from sqlalchemy.sql.schema import ForeignKeyConstraint
 from sqlalchemy.sql.sqltypes import DateTime
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./restaurant.db"                              
-                                                                                
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = "sqlite:///./restaurant.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={
+        "check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()     
+Base = declarative_base()
 
 
+class User(Base):
+    __tablename__ = "user"
 
-class User(Base):                                                                       
-    __tablename__ = "user"                                                              
-
-    id=Column(Integer,primary_key=True)                                                                                  
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     email = Column(String)
     password = Column(String)
-    
+
 
 class Restaurant(Base):
-    __tablename__="restrnt"    
-    
-    id=Column(Integer,primary_key=True)
+    __tablename__ = "restrnt"
+
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     address = Column(String)
     open_time = Column(String)
     close_time = Column(String)
-    userid= Column(Integer, ForeignKey("user.id") )
-    user=relationship(User)
+    userid = Column(Integer, ForeignKey("user.id"))
+    user = relationship(User)
+
 
 class Menu_Category(Base):
-    __tablename__="menu_cat" 
+    __tablename__ = "menu_cat"
 
-    id=Column(Integer,primary_key=True)
-    name=  Column(String) 
-    description = Column(String)   
-    restid = Column(Integer,ForeignKey("restrnt.id"))
-    restrnt=relationship(Restaurant)
-
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    description = Column(String)
+    restid = Column(Integer, ForeignKey("restrnt.id"))
+    restrnt = relationship(Restaurant)
 
 
 class Menu_Item(Base):
-    __tablename__="menu_item" 
-    
-    id=Column(Integer,primary_key=True)
-    categid= Column(Integer,ForeignKey("menu_cat.id")) 
-    name=  Column(String) 
+    __tablename__ = "menu_item"
+
+    id = Column(Integer, primary_key=True)
+    categid = Column(Integer, ForeignKey("menu_cat.id"))
+    name = Column(String)
     description = Column(String)
-    cost=Column(Integer)
-    menu_cat=relationship(Menu_Category)
+    cost = Column(Integer)
+    menu_cat = relationship(Menu_Category)
--- Models/models.py
+++ Models/models.py
@@ -1,69 +1,70 @@
 from typing import Optional
 from sqlalchemy.sql.elements import Null
 from sqlalchemy.sql.sqltypes import NullType
-from fastapi import FastAPI, Depends, Request                                             
-from pydantic import BaseModel                                      
+from fastapi import FastAPI, Depends, Request
+from pydantic import BaseModel
 from sqlalchemy.orm import Session, session
 import datetime
 import sys
 
 
-class UserDetails(BaseModel):                                         
-    
-    
-    name : str
-    email : str
-    password : str
-    
+class UserDetails(BaseModel):
 
-class RestDetails(BaseModel):                                         
-    
-    
-    name : str
-    address : str
-    open_time : str
-    close_time : str
+    name: str
+    email: str
+    password: str
+
+
+class RestDetails(BaseModel):
+
+    name: str
+    address: str
+    open_time: str
+    close_time: str
     userid: int
 
-class ItemDetails(BaseModel):                                         
-    
-    
-    categid : int
-    name : str 
-    description : str
-    cost : int
 
-class CategDetails(BaseModel):                                         
-    
-    name : str 
-    description : str   
-    restid : int 
+class ItemDetails(BaseModel):
+
+    categid: int
+    name: str
+    description: str
+    cost: int
+
+
+class CategDetails(BaseModel):
+
+    name: str
+    description: str
+    restid: int
+
 
 class UserEdit(UserDetails):
-    
-    name : Optional[str]
-    email : Optional[str]
-    password : Optional[str]
-   
+
+    name: Optional[str]
+    email: Optional[str]
+    password: Optional[str]
+
 
 class RestEdit(RestDetails):
-    
-    name : Optional[str]
-    address : Optional[str]
-    open_time : Optional[str]
-    close_time : Optional[str]
-    userid : Optional[int]
+
+    name: Optional[str]
+    address: Optional[str]
+    open_time: Optional[str]
+    close_time: Optional[str]
+    userid: Optional[int]
+
 
 class ItemEdit(ItemDetails):
-    
-    categid : Optional[int]
-    name : Optional[str] 
-    description : Optional[str]
-    cost : Optional[int]
+
+    categid: Optional[int]
+    name: Optional[str]
+    description: Optional[str]
+    cost: Optional[int]
+
 
 class CategEdit(CategDetails):
-    
-    name : Optional[str] 
-    description : Optional[str]   
-    restid : Optional[int] 
-      
+
+    name: Optional[str]
+    description: Optional[str]
+    restid: Optional[int]
